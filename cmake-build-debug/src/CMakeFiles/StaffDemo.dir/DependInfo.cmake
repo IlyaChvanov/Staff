55b07ca5c9a7d280115e764762737ac030ba44b2
Ilya Chvanov <ivchvanov@edu.hse.ru>
2024-05-11 21:18:36 +0300

    calculating salary in main.cpp

---
+++ cmake-build-debug/src/CMakeFiles/StaffDemo.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/user/Desktop/Kostin_cpp/StaffDemo/src/Employee.cpp" "src/CMakeFiles/StaffDemo.dir/Employee.cpp.obj" "gcc" "src/CMakeFiles/StaffDemo.dir/Employee.cpp.obj.d"
+  "C:/Users/user/Desktop/Kostin_cpp/StaffDemo/src/Engineer.cpp" "src/CMakeFiles/StaffDemo.dir/Engineer.cpp.obj" "gcc" "src/CMakeFiles/StaffDemo.dir/Engineer.cpp.obj.d"
+  "C:/Users/user/Desktop/Kostin_cpp/StaffDemo/src/Factory.cpp" "src/CMakeFiles/StaffDemo.dir/Factory.cpp.obj" "gcc" "src/CMakeFiles/StaffDemo.dir/Factory.cpp.obj.d"
+  "C:/Users/user/Desktop/Kostin_cpp/StaffDemo/src/Personal.cpp" "src/CMakeFiles/StaffDemo.dir/Personal.cpp.obj" "gcc" "src/CMakeFiles/StaffDemo.dir/Personal.cpp.obj.d"
+  "C:/Users/user/Desktop/Kostin_cpp/StaffDemo/src/Programmer.cpp" "src/CMakeFiles/StaffDemo.dir/Programmer.cpp.obj" "gcc" "src/CMakeFiles/StaffDemo.dir/Programmer.cpp.obj.d"
+  "C:/Users/user/Desktop/Kostin_cpp/StaffDemo/src/ProjectManager.cpp" "src/CMakeFiles/StaffDemo.dir/ProjectManager.cpp.obj" "gcc" "src/CMakeFiles/StaffDemo.dir/ProjectManager.cpp.obj.d"
+  "C:/Users/user/Desktop/Kostin_cpp/StaffDemo/src/SeniorManager.cpp" "src/CMakeFiles/StaffDemo.dir/SeniorManager.cpp.obj" "gcc" "src/CMakeFiles/StaffDemo.dir/SeniorManager.cpp.obj.d"
+  "C:/Users/user/Desktop/Kostin_cpp/StaffDemo/src/TeamLeader.cpp" "src/CMakeFiles/StaffDemo.dir/TeamLeader.cpp.obj" "gcc" "src/CMakeFiles/StaffDemo.dir/TeamLeader.cpp.obj.d"
+  "C:/Users/user/Desktop/Kostin_cpp/StaffDemo/src/Tester.cpp" "src/CMakeFiles/StaffDemo.dir/Tester.cpp.obj" "gcc" "src/CMakeFiles/StaffDemo.dir/Tester.cpp.obj.d"
+  "C:/Users/user/Desktop/Kostin_cpp/StaffDemo/src/main.cpp" "src/CMakeFiles/StaffDemo.dir/main.cpp.obj" "gcc" "src/CMakeFiles/StaffDemo.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/src/CMakeFiles/StaffDemo.info.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StaffDemo.info.dir/main.cpp.obj"
+  "CMakeFiles/StaffDemo.info.dir/main.cpp.obj.d"
+  "StaffDemo.info.exe"
+  "StaffDemo.info.exe.manifest"
+  "StaffDemo.info.pdb"
+  "libStaffDemo.info.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StaffDemo.info.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
